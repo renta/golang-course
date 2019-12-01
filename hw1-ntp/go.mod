cf6bec7ca1a00d1b0182d9f6efa68f82640d200f
Vasiliy Toporov <toporovvv@gmail.com>
2019-12-02 00:23:06 +0300

    Quick and dummy realization of ntp request.

---
+++ hw1-ntp/go.mod
@@ -0,0 +1,9 @@
+module github.com/renta/golang-course/hw1-ntp
+
+go 1.13
+
+require (
+	github.com/beevik/ntp v0.2.0
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20191126235420-ef20fe5d7933 // indirect
+)
---
+++ hw1-ntp/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
+
+const ntpHost string  = "2.ru.pool.ntp.org"
+
+func main() {
+	fmt.Printf("begin to ask for ntp time to a host %s \n", ntpHost)
+
+	ntpTime, err := ntp.Time(ntpHost)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error after query to ntp host %s, message: %s \n", ntpHost, err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Printf("current time is %s \n", ntpTime)
+	fmt.Println("and formatted")
+	fmt.Printf("current formatted (with RFC3339) time: %s \n", ntpTime.Format(time.RFC3339))
+}
